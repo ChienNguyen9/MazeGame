890ffbb1608d6e958e3a0e1878c40c442680b403
Chien Nguyen <cmnguyen999@yahoo.com>
2016-08-19 17:46:45 -0500

    Added a random level

--- ACSII Maze/GenerateLevel.cpp
+++ ACSII Maze/GenerateLevel.cpp
@@ -7,3 +7,27 @@
 //
 
 #include <stdio.h>
+#include <cstdlib>
+#include <ctime>
+#include <cstdio>
+#include "GenerateLevel.h"
+
+GenerateLevel::GenerateLevel(){
+    
+}
+
+std::string GenerateLevel::levelNumber(){
+    int number = 0;
+    srand(time(NULL));
+    number = rand()%5 + 1;
+    if(number == 1)
+        return "1.txt";
+    else if(number == 2)
+        return "2.txt";
+    else if(number == 3)
+        return "3.txt";
+    else if(number == 4)
+        return "4.txt";
+    else
+        return "5.txt";
+}
