2e5b0f5a83406389dbcb4408ad977424b89b1061
Chien Nguyen <cmnguyen999@yahoo.com>
2016-08-19 17:45:45 -0500

    Create several more maze

---
+++ ACSII Maze/GenerateLevel.cpp
@@ -0,0 +1,9 @@
+//
+//  GenerateLevel.cpp
+//  ACSII Maze
+//
+//  Created by zCatchMee on 8/19/16.
+//  Copyright © 2016 OPz. All rights reserved.
+//
+
+#include <stdio.h>
---
+++ ACSII Maze/GenerateLevel.h
@@ -0,0 +1,13 @@
+//
+//  GenerateLevel.h
+//  ACSII Maze
+//
+//  Created by zCatchMee on 8/19/16.
+//  Copyright © 2016 OPz. All rights reserved.
+//
+
+#ifndef GenerateLevel_h
+#define GenerateLevel_h
+
+
+#endif /* GenerateLevel_h */
